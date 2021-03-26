804361ed15b76059162f4cc1d8de005951b18ff6
Geojorge <jorgezevallosr@gmail.com>
2021-03-26 00:11:54 -0500

    fetch

--- semana6/dia4/01-fetch.js
+++ semana6/dia4/01-fetch.js
@@ -21,21 +21,28 @@ fetch(API).then((peticion) => {
 const apiPeliculas = "https://api.themoviedb.org/3/movie/popular?api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es-ES&page=1"
 const apiGeneros = "https://api.themoviedb.org/3/genre/movie/list?api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es-ES"
 
-// fetch(apiPeliculas).then((pet1) =>{
-//     pet1.json().then((peliculas) =>{
-//         fetch(apiPeliculas).then((pet2) =>{
-//             pet2.json().then((generos) =>{
-//                 console.log(peliculas);
-//                 console.log(generos);
-//             }) 
-//             }
-//         )
-//     })
-// )
+//Problema se hace extenso
+fetch(apiPeliculas).then((pet1) =>{
+    pet1.json().then((peliculas) =>{
+        fetch(apiGeneros).then((pet2) =>{
+            pet2.json().then((generos) =>{
+                console.log(peliculas);
+                console.log(generos);
+            }) 
+            })
+    })
+})
 
+
+
+/** Encadenamiento de promesas */
 fetch(apiPeliculas).then((peticion) => {
     return peticion.json();
-}).then((peliculas) =>{
+  }).then((peliculas) => {
+    console.log(peliculas);
     return fetch(apiGeneros);
-});
-
+  }).then((peticion2) => {
+    return peticion2.json();
+  }).then((generos) => {
+    console.log(generos);
+  })
