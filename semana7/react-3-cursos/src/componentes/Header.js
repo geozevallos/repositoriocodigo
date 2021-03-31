c660f1b64cc0aa47baf8b486d4ffac6223a38847
Geojorge <jorgezevallosr@gmail.com>
2021-03-31 01:10:42 -0500

    Manejo de estados

--- semana7/react-3-cursos/src/componentes/Header.js
+++ semana7/react-3-cursos/src/componentes/Header.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./../assets/logo.png"
 
 const Header = () => {
+
+const [auth, setAuth] = useState(false);
+
   return (
     <header className="header">
       <figure className="header__logo">
         <img src={logo} alt="" />
       </figure>
       <div className="header__auth">
-        <span>Jorge Zevallos</span>
-        <button>Iniciar Sesión</button>
+        
+        {
+          auth ?
+          <button onClick={()=>{
+            setAuth(false);
+          }}>Iniciar Sesión</button> :
+          <>
+          <span>Jorge Zevallos</span>
+          <button onClick={()=>{
+            setAuth(true);
+          }}>Cerrar Sesión</button>
+          </>
+        }
+        
+        
       </div>
     </header>
   );
--- semana7/react-3-cursos/src/componentes/Main.js
+++ semana7/react-3-cursos/src/componentes/Main.js
@@ -1,15 +1,59 @@
-import React from "react";
-import Curso from "./Curso"
+import React, {useState} from "react";
+import Curso from "./Curso";
+import CursoSeleccionado from "./CursoSeleccionado";
+import Carrito from "./Carrito";
 
 const Main = (props) => {
+  const [cursoSelected, setCursoSelected] = useState(null);
+
+  const [carrito, setCarrito] = useState([]);
+
+
+
+  const miFuncion = (objCurso) => {
+    setCursoSelected(objCurso);
+  }
+
+
+
+  //asi era al principio
+  // const agregarAlCarrito = (objCurso) => {
+  //   setCarrito([...carrito, objCurso]);
+    // sería lo mismo que
+    /**
+     * let carritoTemporal = [...carrito];
+     * carritoTemporal.push(objCurso);
+     * setCarrito(carritotemporal)
+     */
+//  }
+
+const agregarAlCarrito = (objCurso) =>{
+//   let cursoExistente = carrito.find((curso) => {
+//     if (curso.id === objCurso.id) {
+//       return curso;
+//     }
+// })
+let cursoExistente = carrito.find(curso => curso.id === objCurso.id)
+if (cursoExistente) return;
+setCarrito([...carrito, objCurso])
+
+}
 
   return (
-    <main className="container">
-      {
-          props.listaCursos.map((objCurso) => {
-              return <Curso objCurso={objCurso}/>
-          })
-      }
+    <main className="main">
+      <CursoSeleccionado cursoSelected={cursoSelected}/>
+      <section className="cursos">
+        {props.listaCursos.map((objCurso) => {
+          return <Curso objCurso={objCurso} 
+          funcion = {miFuncion}
+          agregarAlCarrito = {agregarAlCarrito}
+          key={objCurso.id} />;
+        })}
+      </section>
+
+<Carrito carrito={carrito}/>
+
+
     </main>
   );
 };
